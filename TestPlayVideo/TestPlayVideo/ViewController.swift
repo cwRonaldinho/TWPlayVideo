6f793e062a15111a1d4d7e0ab14301b1d7202d03
cwronaldinho <269221745@qq.com>
2017-07-26 21:28:21 +0800

    test

---
+++ TestPlayVideo/TestPlayVideo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestPlayVideo
+//
+//  Created by tw on 17/7/26.
+//  Copyright © 2017年 tw. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
